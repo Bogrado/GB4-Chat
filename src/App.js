edc4378546aa1c41ec33b6b4dadc1ab3947013f1
Bogrado <andreytrinuma@gmail.com>
2021-10-19 02:50:40 +0700

    Урок 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {
+    useState
+} from 'react';
 import './App.css';
-import Message from "./Message";
-
-const text = "lo rem";
+import Message from './Message';
 
 function App() {
+    const [messageList, setMessageList] = useState([]);
+    const [input, setInput] = useState('')
+    function onClickHandler() {
+        setMessageList (messageList => [...messageList, { author: 'User', text: input }])
+        setInput('');
+    }
+    function onMessageRendered() {
+
+        setMessageList (messageList => [...messageList, { author: 'Bot', text: 'lorem' }])
+    }
+    // 19 строка msg: отправляю целиком и автора и текст (передаю весь объект)
     return (
         <div>
-            <Message text = {text}/>
+            {messageList.map((message, i) => <Message msg={message} key={i} onRendered={onMessageRendered} />)}
+            <input value={input} onInput={e => setInput(e.target.value)}/>
+            <button onClick={onClickHandler}>Test</button>
         </div>
     );
 }
+
 export default App;
--- src/Message.js
+++ src/Message.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React,{
+    useEffect
+} from 'react';
 import './Message.css';
 
 function Message(props) {
+    useEffect(()=> {
+        if (props.msg.author === 'Bot') return
+        setTimeout(props.onRendered, 1500)
+    }, [])
     return (
         <div className="Message">
             <header className="Message-header">
-                message component
+                {props.msg.author}
             </header>
-            <h3 className="messageText">message: {props.text}</h3>
+            <h3 className="messageText">message: {props.msg.text}</h3>
         </div>
     );
 }
-
+Message.propTypes = {
+    msg: PropTypes.object,
+    onRendered: PropTypes.function
+};
 export default Message;
